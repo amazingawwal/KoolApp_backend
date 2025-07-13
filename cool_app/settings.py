f2420eb59dd707184ba94c107bc4320c9562471d
amazingawwal <sakeebmawwal@gmail.com>
2025-07-13 23:26:51 +0100

    include and base template

--- cool_app/settings.py
+++ cool_app/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'cool_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.activity),
+    path('', views.activity, name='home'),
     path('<month>', views.render_html),   
     path('<int:month>', views.activities2),
     path('<str:month>', views.activities, name='monthly-activity'),
